fecf667630ca9ed8a730e55942bbd6b9b07182f7
ogrigriii <57384448+Coachsteveee@users.noreply.github.com>
2024-09-01 16:14:14 +0200

    Update runner.bat

--- runner.bat
+++ runner.bat
@@ -4,11 +4,8 @@ REM #---------------------Running the scraper
 python scraper.py
 REM #---------------------finished running scraper
 
-REM #---------------------Running the Streamlit app
-streamlit run streamlit.py
-
-REM #---------------------Git commands to push the CSV file
+REM push the CSV file
+git pull
 git add fpl_standings.csv
 git commit -m "Update FPL standings"
 git push origin main
-
